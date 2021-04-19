c9afc9ae9e9d58f21de345330ca2eabf50e976d2
Stephen Brown-Bourne <sbrownbourne@gmail.com>
2021-04-19 00:14:58 -0700

    initial table component

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import { useQuery } from 'react-query'
+import type { CellProps, Column, HeaderProps } from 'react-table'
+import { useSortBy, useTable } from 'react-table'
 import styled from 'styled-components'
 
 const Wrapper = styled.div``
@@ -7,13 +9,37 @@ const Wrapper = styled.div``
 type Card = {
   id: number
   name: string
+  creditLimit: number
+  totalSpend: number
+  minimumSpendingRequirement: number
 }
+
+type Cards = Card[]
 
 const currentUser = '1'
 
-const getCardsForUser = async (userId: string) => {
+const columns: Column<Card>[] = [
+  {
+    Header: 'Name',
+    accessor: 'name',
+  },
+  {
+    Header: 'Limit',
+    accessor: 'creditLimit',
+  },
+  {
+    Header: 'Total Spend',
+    accessor: 'totalSpend',
+  },
+  {
+    Header: 'Min. Spending Requirement',
+    accessor: 'minimumSpendingRequirement',
+  },
+]
+
+const getUsersCards = async () => {
   const result = await window.fetch(
-    `http://localhost:3000/user/${userId}/cards`,
+    `http://localhost:3000/user/${currentUser}/cards`,
     {
       method: 'GET',
       headers: {
@@ -25,27 +51,55 @@ const getCardsForUser = async (userId: string) => {
   return json
 }
 
-/* const getUser = async () => { */
-/*   const result = await window.fetch('http://localhost:3000/user/1', { */
-/*     method: 'GET', */
-/*     headers: { */
-/*       /1* 'Access-Control-Allow-Origin': '*', *1/ */
-/*     }, */
-/*   }) */
-/*   console.log(result) */
-/*   const json = await result.json() */
-/*   console.log(json) */
-/*   return json */
-/* } */
+const CardsTable = ({
+  columns,
+  data,
+}: {
+  columns: Column<Card>[]
+  data: Cards
+}) => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable<Card>({ columns: columns, data: data })
+
+  return (
+    <div {...getTableProps()} className="table">
+      <div className="header">
+        {headerGroups.map((headerGroup) => {
+          return (
+            <div {...headerGroup.getHeaderGroupProps()} className="tr">
+              {headerGroup.headers.map((column) => {
+                return column.render('Header')
+              })}
+            </div>
+          )
+        })}
+      </div>
+      <div {...getTableBodyProps()}>
+        {rows.map((row) => {
+          return (
+            <div className="row" {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <div {...cell.getCellProps()}>{cell.render('Cell')}</div>
+              })}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
 
 export default function Dashboard() {
-  const { data: cards, status } = useQuery<Card[]>(
-    'cards',
-    () => getCardsForUser(currentUser),
-    {
-      refetchOnWindowFocus: false,
-    }
-  )
+  const { data: cards, status } = useQuery<Card[]>('cards', getUsersCards, {
+    refetchOnWindowFocus: false,
+  })
+
+  const memoizedCards = useMemo(() => cards, [cards])
 
   if (status === 'loading' || cards === undefined) {
     return null
@@ -63,3 +117,16 @@ export default function Dashboard() {
     </Wrapper>
   )
 }
+
+/* const getUser = async () => { */
+/*   const result = await window.fetch('http://localhost:3000/user/1', { */
+/*     method: 'GET', */
+/*     headers: { */
+/*       /1* 'Access-Control-Allow-Origin': '*', *1/ */
+/*     }, */
+/*   }) */
+/*   console.log(result) */
+/*   const json = await result.json() */
+/*   console.log(json) */
+/*   return json */
+/* } */
