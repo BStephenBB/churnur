fc7a9ffe67a483337d6906623d64f6b23c1356ad
Stephen Brown-Bourne <sbrownbourne@gmail.com>
2021-04-24 01:53:56 -0700

    added more post options

--- server/src/routes/card.ts
+++ server/src/routes/card.ts
@@ -7,28 +7,52 @@ export default async function card(
   app: FastifyInstance,
   options: FastifyServerOptions
 ) {
-  app.post<{ Body: { userId: number; cardName: string } }>(
+  app.post<{
+    Body: {
+      userId: number
+      cardName: string
+      creditLimit?: number
+      totalSpend?: number
+      minimumSpendingRequirement?: number
+      signupBonusDueDate?: string
+    }
+  }>(
     '/card',
     {
       schema: {
         body: {
           type: 'object',
           properties: {
-            userId: { type: 'number' },
-            cardName: { type: 'string' },
+            userId: { type: 'number', required: true },
+            cardName: { type: 'string', required: true },
+            creditLimit: { type: 'number', required: false },
+            totalSpend: { type: 'number', required: false },
+            minimumSpendingRequirement: { type: 'number', required: false },
+            signupBonusDueDate: { type: 'string', required: false },
           },
         },
       },
     },
     async (request, reply) => {
       // TODO user id won't be needed once we attach the user to each request by default
-      const { userId, cardName } = request.body // TODO rest of card options here, figure out how to save dates
+      const {
+        userId,
+        cardName,
+        creditLimit,
+        totalSpend,
+        minimumSpendingRequirement,
+        signupBonusDueDate,
+      } = request.body // TODO rest of card options here, figure out how to save dates
       const { prisma } = app
 
       const newCard = await prisma.card.create({
         data: {
           ownerId: userId,
           name: cardName,
+          creditLimit: creditLimit,
+          totalSpend: totalSpend,
+          minimumSpendingRequirement: minimumSpendingRequirement,
+          signupBonusDueDate: signupBonusDueDate,
         },
       })
 
