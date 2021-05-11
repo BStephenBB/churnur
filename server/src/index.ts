daf51313dc16eebda5fcd1bb82fdc5fa259005a5
Stephen Brown-Bourne <sbrownbourne@gmail.com>
2021-05-11 00:56:48 -0700

    added sus procfile

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
+import App from './app'
 
 const app = fastify({ logger: true }) // only pretty print in dev
 
-app.register(import('./app.js'))
+// app.register(import('./app'))
+app.register(App)
 
 const port = process.env.PORT || '3000'
 app.listen(port)
