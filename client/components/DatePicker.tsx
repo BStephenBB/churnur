474fde418972361444053125d24044d29e1b27d9
Stephen Brown-Bourne <sbrownbourne@gmail.com>
2021-05-29 02:33:15 -0700

    styling for calendar

--- client/components/DatePicker.tsx
+++ client/components/DatePicker.tsx
@@ -8,13 +8,40 @@ import {
 import type { Calendar } from 'dayzed'
 import styled from 'styled-components'
 
-const DateButton = styled.button`
-  width: 50px;
-  height: 50px;
-  border: none;
+const CalendarWrapper = styled.div`
+  display: grid;
+  grid-template-rows: repeat(6, ${({ theme }) => theme.space(12)});
+  grid-template-columns: repeat(7, ${({ theme }) => theme.space(12)});
+`
+
+const Wrapper = styled.div`
+  padding: ${({ theme }) => theme.space(3)};
+  box-shadow: ${({ theme }) => theme.shadow.large};
+  display: inline-block;
+`
+
+const TopWrapper = styled.div`
+  display: flex;
+`
+
+const DateButton = styled.button<{ selected: boolean; today: boolean }>`
   border-radius: 100%;
+  color: ${({ theme }) => theme.color.text};
+  transition: all 0.15s ease;
+  font-variation-settings: 'wght' ${(props) => (props.selected ? '600' : '500')};
+  border: 2px solid ${({ theme }) => theme.color.white};
+  background: ${({ selected, today, theme }) =>
+    selected
+      ? theme.color.blue2
+      : today
+      ? theme.color.green2
+      : theme.color.white};
   &:hover {
-    border: 1px solid ${({ theme }) => theme.color.blue4};
+    border-color: ${({ theme, today, selected }) =>
+      today || selected ? undefined : theme.color.gray6};
+  }
+  &:active {
+    border-color: ${({ theme }) => theme.color.blue4};
   }
 `
 
@@ -32,7 +59,7 @@ const monthNamesShort = [
   'Nov',
   'Dec',
 ]
-const weekdayNamesShort = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+const weekdayNamesShort = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 
 function CalendarUi({
   calendars,
@@ -47,80 +74,68 @@ function CalendarUi({
 }) {
   if (calendars.length) {
     return (
-      <div
-        style={{
-          maxWidth: 800,
-          textAlign: 'center',
-          border: '1px solid blue',
-        }}
-      >
-        <div>
-          <button {...getBackProps({ calendars })}>Back</button>
-          <button {...getForwardProps({ calendars })}>Next</button>
-        </div>
+      <>
         {calendars.map((calendar) => (
-          <div
-            key={`${calendar.month}${calendar.year}`}
-            style={{
-              display: 'inline-block',
-              width: '50%',
-              padding: '0 10px 30px',
-              border: '1px solid red',
-              boxSizing: 'border-box',
-            }}
-          >
-            <div>
-              {monthNamesShort[calendar.month]} {calendar.year}
-            </div>
-            {weekdayNamesShort.map((weekday) => (
-              <div
-                key={`${calendar.month}${calendar.year}${weekday}`}
-                style={{
-                  display: 'inline-block',
-                  width: 'calc(100% / 7)',
-                  border: 'none',
-                  background: 'transparent',
-                }}
-              >
-                {weekday}
+          <Wrapper key={`${calendar.month}${calendar.year}`}>
+            <TopWrapper>
+              <button {...getBackProps({ calendars })}>Back</button>
+              <div>
+                {monthNamesShort[calendar.month]} {calendar.year}
               </div>
-            ))}
-            {calendar.weeks.map((week, weekIndex) =>
-              week.map((dateObj, index) => {
-                let key = `${calendar.month}${calendar.year}${weekIndex}${index}`
-                if (!dateObj) {
+              <button {...getForwardProps({ calendars })}>Next</button>
+            </TopWrapper>
+            <div style={{ width: '100%', display: 'flex' }}>
+              {weekdayNamesShort.map((weekday, index) => (
+                <div
+                  key={`${calendar.month}${calendar.year}${index}`}
+                  style={{
+                    display: 'inline-block',
+                    textAlign: 'center',
+                    width: '48px',
+                    border: 'none',
+                    background: 'transparent',
+                  }}
+                >
+                  {weekday}
+                </div>
+              ))}
+            </div>
+            <CalendarWrapper>
+              {calendar.weeks.map((week, weekIndex) =>
+                week.map((dateObj, index) => {
+                  let key = `${calendar.month}${calendar.year}${weekIndex}${index}`
+                  if (!dateObj) {
+                    return (
+                      <div
+                        key={key}
+                        style={{
+                          display: 'inline-block',
+                          width: 'calc(100% / 7)',
+                          border: 'none',
+                          background: 'transparent',
+                        }}
+                      />
+                    )
+                  }
+                  const { date, selected, selectable, today } = dateObj
+                  let background = today ? 'cornflowerblue' : ''
+                  background = selected ? 'purple' : background
                   return (
-                    <div
+                    <DateButton
+                      selected={selected}
+                      today={today}
                       key={key}
-                      style={{
-                        display: 'inline-block',
-                        width: 'calc(100% / 7)',
-                        border: 'none',
-                        background: 'transparent',
-                      }}
-                    />
+                      {...getDateProps({ dateObj })}
+                    >
+                      {selectable ? date.getDate() : 'X'}
+                    </DateButton>
                   )
-                }
-                const { date, selected, selectable, today } = dateObj
-                let background = today ? 'cornflowerblue' : ''
-                background = selected ? 'purple' : background
-                background = !selectable ? 'teal' : background
-                return (
-                  <DateButton
-                    style={{
-                      background,
-                    }}
-                    key={key}
-                    {...getDateProps({ dateObj })}
-                  >
-                    {selectable ? date.getDate() : 'X'}
-                  </DateButton>
-                )
-              })
-            )}
-          </div>
+                })
+              )}
+            </CalendarWrapper>
+          </Wrapper>
         ))}
-      </div>
+      </>
     )
   }
   return null
@@ -152,12 +167,12 @@ export const SingleDate = () => {
         selected={selectedDate}
         onDateSelected={_handleOnDateSelected}
       />
-      {selectedDate && (
-        <div style={{ paddingTop: 20, textAlign: 'center' }}>
-          <p>Selected:</p>
-          <p>{`${selectedDate?.toLocaleDateString()}`}</p>
-        </div>
-      )}
     </div>
   )
 }
+/* {selectedDate && ( */
+/*       <div style={{ paddingTop: 20, textAlign: 'center' }}> */
+/*       <p>Selected:</p> */
+/*       <p>{`${selectedDate?.toLocaleDateString()}`}</p> */
+/*       </div> */
+/* )} */
