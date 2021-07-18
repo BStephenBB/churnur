15f5bf8566b0154a6986af3c15f033496c9e57b7
Stephen Brown-Bourne <sbrownbourne@gmail.com>
2021-07-18 00:06:50 -0700

    got mail to send

--- server/src/routes/spendDueDateReminder.ts
+++ server/src/routes/spendDueDateReminder.ts
@@ -1,77 +1,77 @@
-import type { FastifyInstance, FastifyServerOptions } from 'fastify'
+// import type { FastifyInstance, FastifyServerOptions } from 'fastify'
 
-type Params = {
-  id: string
-}
+// type Params = {
+//   id: string
+// }
 
-export default async function spendDueDateReminder(
-  app: FastifyInstance,
-  options: FastifyServerOptions
-) {
-  app.get<{ Params: Params }>(
-    '/remind-spend-due',
-    {
-      schema: {
-        response: {
-          200: {
-            type: 'object',
-            properties: {
-              message: { type: 'string' },
-            },
-          },
-        },
-      },
-    },
-    async (request, reply) => {
-      //
-      const { prisma } = app
-      const { id } = request.params
+// export default async function spendDueDateReminder(
+//   app: FastifyInstance,
+//   options: FastifyServerOptions
+// ) {
+//   app.get<{ Params: Params }>(
+//     '/remind-spend-due',
+//     {
+//       schema: {
+//         response: {
+//           200: {
+//             type: 'object',
+//             properties: {
+//               message: { type: 'string' },
+//             },
+//           },
+//         },
+//       },
+//     },
+//     async (request, reply) => {
+//       //
+//       const { prisma } = app
+//       const { id } = request.params
 
-      const result = await prisma.user.findUnique({
-        where: {
-          id: Number(id),
-        },
-      })
+//       const result = await prisma.user.findUnique({
+//         where: {
+//           id: Number(id),
+//         },
+//       })
 
-      reply.send(result)
-    }
-  )
-}
+//       reply.send(result)
+//     }
+//   )
+// }
 
-type Params = {
-  id: string
-}
+// type Params = {
+//   id: string
+// }
 
-export default async function spendDueDateReminder(
-  app: FastifyInstance,
-  options: FastifyServerOptions
-) {
-  app.get<{ Params: Params }>(
-    '/remind-spend-due',
-    {
-      schema: {
-        response: {
-          200: {
-            type: 'object',
-            properties: {
-              message: { type: 'string' },
-            },
-          },
-        },
-      },
-    },
-    async (request, reply) => {
-      //
-      const { prisma } = app
-      const { id } = request.params
+// export default async function spendDueDateReminder(
+//   app: FastifyInstance,
+//   options: FastifyServerOptions
+// ) {
+//   app.get<{ Params: Params }>(
+//     '/remind-spend-due',
+//     {
+//       schema: {
+//         response: {
+//           200: {
+//             type: 'object',
+//             properties: {
+//               message: { type: 'string' },
+//             },
+//           },
+//         },
+//       },
+//     },
+//     async (request, reply) => {
+//       //
+//       const { prisma } = app
+//       const { id } = request.params
 
-      const result = await prisma.user.findUnique({
-        where: {
-          id: Number(id),
-        },
-      })
+//       const result = await prisma.user.findUnique({
+//         where: {
+//           id: Number(id),
+//         },
+//       })
 
-      reply.send(result)
-    }
-  )
-}
+//       reply.send(result)
+//     }
+//   )
+// }
--- server/src/tasks/spendDue.ts
+++ server/src/tasks/spendDue.ts
@@ -1,25 +1,68 @@
+// import { MailService as SendMail } from '@sendgrid/mail'
+import SendMail from '@sendgrid/mail'
+import { isSameDay, subWeeks } from 'date-fns'
 import type { PrismaClient as PrismaClientType } from '@prisma/client'
 // TODO look into this to fix this issue: https://stackoverflow.com/questions/44058101/typescript-declare-third-party-modules
 // @ts-ignore: silly lack of common js or support or something
 import pkg from '@prisma/client'
 const { PrismaClient } = pkg
 
-const prisma: PrismaClientType = new PrismaClient()
+// const prisma: PrismaClientType = new PrismaClient()
 
 // for each user, get their cards
 // for each card, check if they have outstanding spend --> if they do, send reminder
-//
 
-const getAllUsers = async function () {
-  // get all users
-  const users = await prisma.user.findMany({
-    include: {
-      cards: true,
-    },
-  })
-  console.log(JSON.stringify(users))
+// const getAllUsers = async function () {
+//   // get all users
+//   const users = await prisma.user.findMany({
+//     include: {
+//       cards: true,
+//     },
+//   })
+//   // console.log(JSON.stringify(users))
 
-  // send test email
+//   for (let i = 0; i < users.length; i++) {
+//     const cards = users[i].cards
+//     const cardsToWarnAbout = []
+//     for (let j = 0; j < cards.length; j++) {
+//       const { totalSpend, minimumSpendingRequirement, signupBonusDueDate } =
+//         cards[j]
+//       if (signupBonusDueDate && totalSpend && minimumSpendingRequirement) {
+//         const today = new Date()
+//         const bonusDate = new Date(signupBonusDueDate)
+//         const twoWeeksBeforeBonusDate = subWeeks(bonusDate, 2)
+//         const isTwoWeeksBefore = isSameDay(today, twoWeeksBeforeBonusDate)
+//         if (totalSpend < minimumSpendingRequirement && isTwoWeeksBefore) {
+//           cardsToWarnAbout.push(cards[j])
+//         }
+//       }
+//     }
+//     console.log(cardsToWarnAbout)
+//     // now that we have all cards for the user w/ those cards and the user we can build the email
+//   }
+// }
+
+const key =
+  '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'
+
+// send test email
+
+SendMail.setApiKey(key)
+const message = {
+  to: 'sbrownbourne@gmail.com',
+  from: 'reminders@churnur.com',
+  subject: 'testing',
+  text: 'hello from node js',
+  html: '<strong>sup</strong>',
 }
 
-getAllUsers()
+SendMail.send(message)
+  .then(() => {
+    console.log('sent mail')
+  })
+  .catch((error: unknown) => {
+    console.error(error)
+  })
+
+// getAllUsers()
+// prisma.$disconnect()
